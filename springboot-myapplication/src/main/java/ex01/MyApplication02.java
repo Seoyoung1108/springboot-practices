abb8c6064bea4027cf3dba1ffe95ffde5f74ecec
BIT <BIT@BIT1>
2025-01-21 10:40:53 +0900

    spring myapplication 01/21

---
+++ springboot-myapplication/src/main/java/ex01/MyApplication02.java
@@ -0,0 +1,22 @@
+package ex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+public class MyApplication02 {
+
+	@Bean("MyComponent02")
+	public MyComponent myComponent() {
+		
+		return new MyComponent();
+	}
+	public static void main(String[] args) {
+		try(ConfigurableApplicationContext ac = SpringApplication.run(MyApplication02.class, args)) {
+			
+		}
+	}
+
+}
